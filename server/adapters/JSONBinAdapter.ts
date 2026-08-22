75a5dcce3919dfaa2e052815ea62cd9a0c2d7c35
BrunoPanassi <bhenrique95@outlook.com>
2026-08-22 18:38:31 -0300

    Refactor JSONBinAdapter constructor
    With this refactor we move one responsibility from BaseRepository to the JSONBinAdapter

--- server/adapters/JSONBinAdapter.ts
+++ server/adapters/JSONBinAdapter.ts
@@ -3,11 +3,43 @@ export class JSONBinAdapter<T extends { id: number }> {
     private baseUrl = "";
     private apiKey = "";
 
-    constructor(binId: string) {
+    constructor(entityName: string) {
+        const binId = this.findBinId(entityName)
         const config = useRuntimeConfig();
         this.baseUrl = config.public.jsonBinBaseUrl as string
         this.apiKey = config.jsonBinApiKey as string
         this.binId = binId;
+    }
+
+    private readonly tableJsonBinIds = [
+        {
+            "table": "users",
+            "binId": '6912585e43b1c97be9a57673'
+        },
+                {
+            "table": "stores",
+            "binId": '6912588c43b1c97be9a576b4'
+        },
+                {
+            "table": "products",
+            "binId": '691258eed0ea881f40e06d73'
+        },
+                {
+            "table": "materials",
+            "binId": '69125914d0ea881f40e06da8'
+        },
+                {
+            "table": "material-types",
+            "binId": '691259d543b1c97be9a5785b'
+        },
+                {
+            "table": "product-store",
+            "binId": '69125a71d0ea881f40e06f3a'
+        }
+    ]
+
+    findBinId(table: string) {
+        return this.tableJsonBinIds.find(val => val.table == table)?.binId ?? ''
     }
 
     private get headers() {
--- server/repositories/BaseRepository.ts
+++ server/repositories/BaseRepository.ts
@@ -4,41 +4,10 @@ import { JSONBinAdapter } from '../adapters/JSONBinAdapter';
 export class BaseRepository<T extends { id: number }> {
     protected adapter: JSONBinAdapter<T> | SqlAdapter<T>;
 
-    private readonly tableJsonBinIds = [
-        {
-            "table": "users",
-            "binId": '6912585e43b1c97be9a57673'
-        },
-                {
-            "table": "stores",
-            "binId": '6912588c43b1c97be9a576b4'
-        },
-                {
-            "table": "products",
-            "binId": '691258eed0ea881f40e06d73'
-        },
-                {
-            "table": "materials",
-            "binId": '69125914d0ea881f40e06da8'
-        },
-                {
-            "table": "material-types",
-            "binId": '691259d543b1c97be9a5785b'
-        },
-                {
-            "table": "product-store",
-            "binId": '69125a71d0ea881f40e06f3a'
-        }
-    ]
-    
     constructor(entityName: string, useSql: boolean = false) {
         this.adapter = useSql
             ? new SqlAdapter<T>(entityName)
-            : new JSONBinAdapter<T>(this.findBinId(entityName));
-    }
-
-    findBinId(table: string) {
-        return this.tableJsonBinIds.find(val => val.table == table)?.binId ?? ''
+            : new JSONBinAdapter<T>(entityName);
     }
 
     async getAll(): Promise<T[]> {
